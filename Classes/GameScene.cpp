22020fa1d9a9d54ee008232276a4f136b6938172
dkzhe <dk_zhe@163.com>
2014-01-26 18:20:52 +0800

    temp

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -5,11 +5,8 @@ GameScene::GameScene(void)
 {
 	m_hellowordLay=NULL;
 	m_PlaneLayer=NULL;
-<<<<<<< HEAD
-//	m_BulletLayer=NULL;
-=======
 	m_BulletLayer=NULL;
->>>>>>> 0fd1d4af53462490df9af9a21c0ed1bb00c3a786
+
 }
 
 
@@ -32,17 +29,10 @@ bool GameScene::init()
 		m_PlaneLayer=PlaneLayer::create();
 		CC_BREAK_IF(!m_PlaneLayer);
 		this->addChild(m_PlaneLayer);
-<<<<<<< HEAD
-/*
-		m_BulletLayer=BulletLayer::create();
-		CC_BREAK_IF(!m_BulletLayer);
-		this->addChild(m_BulletLayer);*/
-=======
 
 		m_BulletLayer=BulletLayer::create();
 		CC_BREAK_IF(!m_BulletLayer);
 		this->addChild(m_BulletLayer);
->>>>>>> 0fd1d4af53462490df9af9a21c0ed1bb00c3a786
 		//m_BulletLayer->StartShoot(); //БФдкетЖЮ
 		bRet=true;
 
